a9b9a492fcace29e5cabe65b7c59890dc3c60037
Mark Alfred Macapagal <mark.alfred072915@gmail.com>
2024-01-15 06:15:36 +0800

    Finalize form

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
     import AppBanner from '@/components/AppBanner.vue';
     import AppNavigationBar from '@/components/AppNavigationBar.vue';
     import AppCard from '@/components/AppCard.vue';
+    import ButtonPrimary from '@/components/ButtonPrimary.vue';
+    import InputText from "@/components/inputs/InputText.vue"
+    import InputTextarea from "@/components/inputs/InputTextarea.vue"
 </script>
 <template>
     <div class="main-wrapper">
@@ -9,18 +12,40 @@
         <AppNavigationBar />
 
         <!-- Main content -->
-        <div class="container mx-auto">
-            <h1 class="text-[32px]">Neem contact op</h1>
-            <div class="flex">
-                <div class="flex-1">
-                    <AppCard>
-                        asd
-                    </AppCard>
+        <div class="px-[100px] py-[40px]">
+            <div class="container mx-auto">
+                <h1 class="text-[32px] mb-8 font-medium">Neem contact op</h1>
+                <div class="flex">
+                    <div class="flex-1">
+                        <AppCard>
+                            <div class="flex flex-row gap-[15px] mb-4">
+                                <div class="w-1/2">
+                                    <InputText label="Naam" placeholder="Naam"/>
+                                </div>
+                                <div class="w-1/2">
+                                    <InputText label="Email" placeholder="Email"/>
+                                </div>
+                            </div>
+                            <div class="flex flex-row gap-[15px] mb-4">
+                                <div class="w-full">
+                                    <InputText label="Onderwerp" placeholder="Onderwerp"/>
+                                </div>
+                            </div>
+                            <div class="flex flex-row gap-[15px] mb-10">
+                                <div class="w-full">
+                                    <InputTextarea label="Bericht" placeholder="Bericht"/>
+                                </div>
+                            </div>
+                            <ButtonPrimary>
+                                Versturen
+                                <img src="@/assets/icons/arrow-right.png" class="ml-2 icon-size">
+                            </ButtonPrimary>
+                        </AppCard>
+                    </div>
+                    <div class="flex-auto"></div>
                 </div>
-                <div class="flex-auto"></div>
             </div>
         </div>
-        
         <!-- End main content -->
     </div>
 </template>
--- src/components/AppNavigationBar.vue
+++ src/components/AppNavigationBar.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="px-[108px] py-4 bg-[#006FEB] flex flex-row justify-around">
-        <a href="#" class="text-white flex flex-row items-center">
+        <a href="#" class="text-white flex flex-row items-center text-sm">
             <img src="@/assets/icons/trophy.png" class="h-[24px] w-[24px] mr-4">
-            <span><b>Voordeligste</b> aanbeider</span>
+            <span><b class="font-medium">Voordeligste</b> aanbeider</span>
         </a>
-        <a href="#" class="text-white flex flex-row items-center">
+        <a href="#" class="text-white flex flex-row items-center  text-sm">
             <img src="@/assets/icons/call.png" class="h-[24px] w-[24px] mr-4">
-            <span><b>24/7</b> noodnummer</span>
+            <span><b class="font-medium">24/7</b> noodnummer</span>
         </a>
-        <a href="#" class="text-white flex flex-row items-center">
+        <a href="#" class="text-white flex flex-row items-center  text-sm">
             <img src="@/assets/icons/delivery.png" class="h-[24px] w-[24px] mr-4">
-            <span><b>Geen</b> extra kosten</span>
+            <span><b class="font-medium">Geen</b> extra kosten</span>
         </a>
-        <a href="#" class="text-white flex flex-row items-center">
+        <a href="#" class="text-white flex flex-row items-center  text-sm">
             <img src="@/assets/icons/people.png" class="h-[24px] w-[24px] mr-4">
-            <span><b>Zitplaatsen</b> bij elkaar</span>
+            <span><b class="font-medium">Zitplaatsen</b> bij elkaar</span>
         </a>
-        <a href="#" class="text-white flex flex-row items-center">
+        <a href="#" class="text-white flex flex-row items-center  text-sm">
             <img src="@/assets/icons/user.png" class="h-[24px] w-[24px] mr-4">
-            <span><b>10000 +</b> tevreden klanten</span>
+            <span><b class="font-medium">10000 +</b> tevreden klanten</span>
         </a>
     </div>
 </template>
